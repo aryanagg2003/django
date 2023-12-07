c6166ab85e80eae67544321829e61f702a991241
aryanagg2003 <aryan.wappnet@gmail.com>
2023-12-07 14:35:25 +0530

    updated task management

---
+++ django first project/taskmanagement/task/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# Create your models here.
---
+++ django first project/taskmanagement/taskmanagement/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for taskmanagement project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from task import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('task-list/',views.task_list,name='task-list'),
+    path('task-create/',views.task_create,name='task-create'),
+    path('task-detail/<int:pk>/',views.task_detail,name='task-detail'),
+    path('task-update/<int:pk>/',views.task_update,name='task-update'),
+    path('task-delete/<int:pk>/',views.task_delete,name='task-delete'),
+]
